c6e765ebe431f65c38fe97823bc137a60780af0f
Bryan Motta <06122001bryan@gmail.com>
2026-04-12 19:07:11 -0500

    Delete Flask - mysql directory

--- Flask - mysql/back-end/db_setup.py
+++
@@ -1,9 +0,0 @@
-# db_setup.py
-from app import create_app
-from models import db
-
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-    print("✅ Database tables created (or already exist).")
